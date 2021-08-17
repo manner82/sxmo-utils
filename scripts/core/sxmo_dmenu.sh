800ba031df47710b39fe8425b814248d6e28ea01
Stacy Harper <contact@stacyharper.net>
2021-08-17 22:58:23 +0200

    Use default bemenu colors

--- scripts/core/sxmo_dmenu.sh
+++ scripts/core/sxmo_dmenu.sh
@@ -3,22 +3,7 @@
 TERMMODE=$([ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] && echo "true")
 
 if [ -z "$BEMENU_OPTS" ]; then
-	export BEMENU_OPTS='
-		-m -1
-		--hb "#222222"
-		--tb "#222222"
-		--fb "#222222"
-		--nb "#222222"
-		--sb "#222222"
-		--hb "#285577"
-		--hf "#222222"
-		--tf "#285577"
-		--nf "#285577"
-		--scb "#222222"
-		--scf "#285577"
-		--ff "#285577"
-		--fn "Monospace 11"
-	'
+	export BEMENU_OPTS='--fn "Monospace 11"'
 fi
 
 if [ "$TERMMODE" != "true" ]; then
