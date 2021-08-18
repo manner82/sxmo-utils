cb381bdf0a40b5f17d827300532af08ed87e753b
Stacy Harper <contact@stacyharper.net>
2021-08-18 12:09:46 +0200

    Simplify the multikey

--- scripts/core/sxmo_multikey.sh
+++ scripts/core/sxmo_multikey.sh
@@ -11,33 +11,22 @@ else
 	counter=1
 fi
 
-printf %s "$counter" > "$count_file"
 shift "$counter"
-if [ "$#" -eq 1 ]; then
-	last_action=1
-elif [ "$#" -eq 0 ]; then
+if [ "$#" -eq 0 ]; then
 	exit
 fi
+printf %s "$counter" > "$count_file"
 
 sleep "$threshold"
 
-if [ ! -f "$count_file" ]; then
+if [ "$counter" != "$(cat "$count_file")" ]; then
 	exit
-fi
-
-new_counter="$(cat "$count_file")"
-if [ "$counter" != "$new_counter" ]; then
-	if [ -n "$last_action" ] && [ "$new_counter" -gt "$counter" ]; then
-		count_overflow=1
-	else
-		exit
-	fi
 fi
 
 eval "$1"
 
-if [ -n "$count_overflow" ]; then
-	sleep "$threshold" # prevent too long long presses to chain
+if [ "$#" -eq 1 ]; then
+	sleep "$threshold" # prevent holded presses to chain
 fi
 
 rm "$count_file"
