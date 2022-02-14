0c70cf5ae0f97df40ef60d1794985480bbf18eb6
Robert Manner <rmanni@gmail.com>
2022-02-14 21:19:18 +0100

    appcfg/sway: use background of user

--- configs/default_hooks/sxmo_hook_start.sh
+++ configs/default_hooks/sxmo_hook_start.sh
@@ -45,7 +45,7 @@ case "$SXMO_WM" in
 		superctl start sxmo_wob
 		superctl start sxmo_menumode_toggler
 		superctl start bonsaid
-		swaymsg output '*' bg "$SXMO_BG_IMG" fill
+		# swaymsg output '*' bg "$SXMO_BG_IMG" fill  # TODO: not sure why it is not working
 		;;
 	dwm)
 		superctl start dunst
