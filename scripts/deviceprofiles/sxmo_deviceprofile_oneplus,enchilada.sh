36d0d4d03fcf892cd41ad2a99c260ef7b959c917
hazardchem <hazardchem@disroot.org>
2024-11-29 23:12:26 +1000

    Add in touchscreen id into OnePlus 6 profile
    This is needed by dwm as screen rotation and screenoff states don't work correctly.
    
    Signed-off-by: hazardchem <hazardchem@disroot.org>
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

--- scripts/deviceprofiles/sxmo_deviceprofile_oneplus,enchilada.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_oneplus,enchilada.sh
@@ -5,6 +5,7 @@
 export SXMO_VOLUME_BUTTON="1:1:Volume_keys"
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
 export SXMO_MONITOR="DSI-1"
+export SXMO_TOUCHSCREEN_ID="Synaptics S3706B"
 export SXMO_VIBRATE_DEV="/dev/input/by-path/platform-c440000.spmi-platform-c440000.spmi:pmic@3:haptics@c000-event"
 export SXMO_VIBRATE_STRENGTH="5000"
 export SXMO_SWAY_SCALE="3"
